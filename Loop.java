6e7dde0fac49667591867506dc32ee139206d25b
Farhan <143472898+shaikfarhan0703@users.noreply.github.com>
2025-05-14 16:22:12 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,48 @@
+import javax.swing.plaf.synth.SynthToggleButtonUI;
+
+// public class Loop{
+//     public static void main(String[] args){
+//         for(int i = 1; i <= 10; i++){
+//                 System.out.print(i + " ");
+//         }
+//     }
+
+// }
+
+// public class Loop{
+//     public static void main(String[] args){
+//         for(int i = 10; i >= 1; i--){
+//                 System.out.print(i + " ");
+//         }
+//     }
+
+// }
+
+// public class Loop{
+//     public static void main(String[] args){
+//         for(int i = 2; i <= 20; i += 2){
+//                 System.out.print(i + " ");
+//         }
+//     }
+
+// }
+
+
+// public class Loop{
+//     public static void main(String[] args){
+//         for(int i = 1; i <= 15; i += 2){
+//                 System.out.print(i + " ");
+//         }
+//     }
+
+// }
+
+
+public class Loop{
+    public static void main(String[] args){
+        for(int i = 5; i <= 50; i += 5){
+                System.out.print(i + " ");
+        }
+    }
+
+}
